6fa0b66385d2456f0ccf7d32cca677728cda1115
28q <79777472+28q@users.noreply.github.com>
2023-11-18 17:50:11 +0900

    Create dlp.cpp

---
+++ math/dlp.cpp
@@ -0,0 +1,49 @@
+//モノイドの型, 集合の型, モノイドの演算, モノイドの作用, モノイドの単位元
+template<class S, class T>
+struct DLP{
+  int m;
+  S f;
+  T s, t;
+  long long n;
+  DLP(S _f, long long N, T _s, T _t):
+    n(N), f(_f), s(_s), t(_t)
+    {m = sqrt(N);}
+  DLP(S _f, long long N, T _s, T _t, long long _m):
+    n(N), f(_f), s(_s), t(_t)
+    {m = _m;}
+  template<S (*op)(S, S), T (*apply)(S, T), S (*e)()> int solve() const {
+    T t2 = t;
+    //unordered_mapにするとlogが落ちる
+    map<T, bool> H;
+    for(int i = 0; i < m; i++){
+      t2 = apply(f, t2);
+      H[t2] = true;
+    }
+    S g = e();
+    S f2 = f;
+    int m2 = m;
+    while(m2){
+      if(m2&1) g = op(g, f2);
+      f2 = op(f2, f2);
+      m2 >>= 1;
+    }
+    //pow(f, m);
+    S g2 = g;
+    S g3 = e();
+    int isok = 2;
+    long long k = 0;
+    while(++k && isok > 0 && (k-1)*m <= n){
+      if(H.contains(apply(g2, s))){
+        isok--;
+        S g4 = g3;
+        for(int i = 0; i <= m; i++){
+          if(apply(g4, s) == t) return (k-1)*m+i;
+          g4 = op(g4, f);
+        }
+      }
+      g2 = op(g2, g);
+      g3 = op(g3, g);
+    }
+    return -1;
+  }
+};
